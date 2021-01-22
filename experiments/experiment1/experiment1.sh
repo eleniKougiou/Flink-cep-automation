bca100bfc1c708383f2f33279262ce549356e57b
Eleni Kougioumtzi <elkoum15@gmail.com>
2021-01-23 01:50:45 +0200

    Improve path to save files

--- experiments/experiment1/experiment1.sh
+++ experiments/experiment1/experiment1.sh
@@ -6,14 +6,14 @@ cd ..
 
 # pattern = aaabbb 
 # stream length = WANTED, number of matches = 1 
-# contiguity = 1 (strict), strategy = 1 (no skip)
+# contiguity = 1 (strict), strategy = 1 (no skip), parallelism = 1
 # input file = "seqWANTED.txt", output file = "resultWANTED.txt"
-# number of experiment = 1
+# path to save generated files = $PWD (Print Working Dir)
 
 # stream length from 10000 to 100000 by 10000
 for (( i=1; i<=10; i++ ))
 do	
 	WANTED=$(( i * 10000 ))
 	echo "Stream length: $WANTED"
-	./run.sh 'aaabbb' $WANTED 1 1 1 "seq"$WANTED".txt" "result"$WANTED".txt" 1
+	./run.sh 'aaabbb' $WANTED 1 1 1 1 "seq"$WANTED".txt" "result"$WANTED".txt" $PWD
 done
