e613c68308691975b9a4ad1d88b930118615953d
Eleni Kougioumtzi <elkoum15@gmail.com>
2021-01-22 02:29:14 +0200

    Update comments (StreamGenerator.py)

--- StreamGenerator.py
+++ StreamGenerator.py
@@ -25,14 +25,12 @@ def inject_one(stream, match):
 
 # more complex version of the above that injects a list of matches with no collisions
 def inject_many(stream, matches):
-    #print('original stream=', stream)
     pos = [0];
     n = len(matches)
     # create random positions where the matches will be added
     for m in matches:
         pos.append(secrets.randbelow(len(stream)))
     pos = sorted(pos)
-    # print(pos)
     out = ''
     for i in range(1, n + 1):
         if verbose:
@@ -40,9 +38,6 @@ def inject_many(stream, matches):
         out = out + stream[pos[i - 1]:pos[i]] + matches[i - 1]
     out += stream[pos[n]:]
     return out
-
-
-# print(inject_many('012',['a','b','c','d']))
 
 # augment a match using the _ character
 # useful when testing no_strict policy
@@ -62,17 +57,15 @@ def augment_match(m, max_aug=10):
 # print debug info
 verbose = True
 
-# arguments: 1 = pattern, 2 = stream_length, 3 = num_matches, 4 = strict, 5 = output file
+# arguments: 1 = pattern, 2 = stream length, 3 = number of matches, 4 = contiguity condition, 5 = path for output file
 pattern = sys.argv[1]
 stream_length = int(sys.argv[2])
 num_matches = int(sys.argv[3])
 strict = int(sys.argv[4]) == 1
-output_file = sys.argv[5] #'seq.txt'
+output_file = sys.argv[5] # path for creating 'seq_.txt'
 
 # initialize a random stream
 stream = randomstream_generator(size=stream_length)
-#if verbose:
-#    print("initial stream: " + stream)
 
 matches = []
 for i in range(num_matches):
@@ -85,10 +78,11 @@ for i in range(num_matches):
     matches.append(s)
 
 generated_stream = inject_many(stream, matches)
-#print(generated_stream)
-f = open(output_file, "w")
-#f.write(generated_stream)
-for event in generated_stream:
-    f.write(event + "\n")
-f.close()
+# print(generated_stream)
 
+# write file to the appropriate experiment folder 
+f =open(output_file, "w")
+
+for event in generated_stream:
+    f.write(event + "\n") # write one character per row
+f.close()
