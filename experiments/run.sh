51c166afcffe8563f6615b0eff83fd69ceaf9f5d
Eleni Kougioumtzi <elkoum15@gmail.com>
2021-01-22 02:44:34 +0200

    Add arg8 and set appropriate paths for the files

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Terminal ./run.sh arg1 arg2 arg3 arg4 arg5 arg6 arg7
+# Terminal ./run.sh arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8
 # arg1 = pattern, arg2 = stream length, arg3 = number of matches
 # arg4 = contiguity, arg5 = strategy, arg6 =input file, arg7 = output file
+# arg8 = number of experiment
 
 # Stop script if any simple command fails
 set -e 
 
 int='^[0-9]+$' 
 
-echo "(correct format: ./runpython.sh arg1 arg2 arg3 arg4 arg5 arg6 arg7) "
+echo "(correct format: ./runpython.sh arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8) "
 echo ""
 
-if [ "$#" -ne 7 ]; then # Wrong number of parameters
+if [ "$#" -ne 8 ]; then # Wrong number of parameters
     	echo "ERROR: Wrong number of parameters. "
 	echo "arg1 = 'wanted pattern' ('String')"
     	echo "arg2 = stream length (int)"
@@ -23,6 +24,7 @@ if [ "$#" -ne 7 ]; then # Wrong number of parameters
     	echo "(1: no skip, 2: skip to next, 3: skip past last event, 4: skip to first, 5: skip to last)"
     	echo "arg6 = 'name of input file' ('String')"
     	echo "arg7 = 'name of output file' ('String')"
+    	echo "arg8 = number of experiment (int)"
 
 elif [[ $1 =~ $int ]] ; then # arg1 is not a string
    	echo "ERROR: arg1 must be a String (give wanted pattern)"
@@ -68,10 +70,23 @@ elif [[ $6 =~ $int ]] ; then # input file is not a string
 elif [[ $7 =~ $int ]] ; then # output file is not a string
    	echo "ERROR: arg7 must be a String (give the name of output file)"
 
+elif ! [[ $8 =~ $int ]] ; then # arg8 is not a number
+   	echo "ERROR: arg8 must be an integer (number of experiment)"
+   	
+elif [ $8 -le 0 ]; then # arg8 is <= 0 
+	echo "ERROR: arg8 must be greater than 0"
+	echo "arg8 = number of experiment"
+
 
 else # correct arguments
+	# save $6 (seq_.txt) file to the appropriate experiment folder (experiment$8)
+	inputPath="$PWD/experiment$8/$6"
+	# save $7 (result_.txt) file to the appropriate experiment folder (experiment$8)
+	outputPath="$PWD/experiment$8/$7"
+	
 	echo "RUN STREAM GENERATOR" 
-	./runpython.sh $1 $2 $3 $4 $6
+	./runpython.sh $1 $2 $3 $4 $inputPath
+
 	echo "RUN FLINK PROGRAM"
-	./runflink.sh 0 $1 $4 $5 $6 $7
+	./runflink.sh $1 $4 $5 $inputPath $outputPath
 fi
