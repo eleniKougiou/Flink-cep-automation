e03cd223d0dc60fc623c2beed87ad2ddb2a67bc7
Eleni Kougioumtzi <elkoum15@gmail.com>
2021-01-22 02:36:53 +0200

    Correct dir

--- experiments/runpython.sh
+++ experiments/runpython.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Terminal ./runpython.sh arg1 arg2 arg3 arg4 arg5
-# arg1 = pattern, arg2 = stream length, arg3 = number of matches, arg4 = contiguity, arg5 =output file
+# Exit current folder, go one directory up (Flink-cep-automation).
+# There you can find StreamGenerator.py
+cd .. 
+
+# Terminal: ./runpython.sh arg1 arg2 arg3 arg4 arg5 arg6
+# arg1 = pattern, arg2 = stream length, arg3 = number of matches, arg4 = contiguity, arg5 = output file, arg6 = number of experiment
 
 python3 StreamGenerator.py "$@"
