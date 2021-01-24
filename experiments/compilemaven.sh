b61d34769f1959465707772248713ed854e11a3b
Eleni Kougioumtzi <elkoum15@gmail.com>
2021-01-24 22:53:16 +0200

    Add jar creation

--- experiments/compilemaven.sh
+++ experiments/compilemaven.sh
@@ -4,5 +4,5 @@
 # There you can find the pom.xml in order to compile the project.
 cd .. 
 
-# Clean & compile maven project 
-mvn clean compile
+# Clean & compile maven project, rebuild jar file
+mvn clean compile assembly:single
