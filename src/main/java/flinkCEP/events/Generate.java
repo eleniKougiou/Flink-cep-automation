ad2337665d0278fe18d3b74dc9166df695f9e082
Eleni Kougioumtzi <elkoum15@gmail.com>
2021-04-23 12:20:22 +0300

    Update Generate.java

--- src/main/java/flinkCEP/events/Generate.java
+++ src/main/java/flinkCEP/events/Generate.java
@@ -22,8 +22,6 @@ import org.apache.flink.cep.nfa.aftermatch.AfterMatchSkipStrategy;
 import org.apache.flink.cep.pattern.Pattern;
 import org.apache.flink.cep.pattern.conditions.SimpleCondition;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -33,33 +31,24 @@ import java.util.*;
 
 public class Generate implements Serializable{
 
-    static String strP; // wanted pattern
-    static int n = 0; // number of events in the pattern
-    static int nr = 0; // number of results
-    static double start = System.currentTimeMillis();
-    static StreamExecutionEnvironment env;
+    private static String strP; // Wanted pattern
+    private static int n = 0; // Number of events in the pattern
+    private static int nr = 0; // Number of results
 
-    static int contiguity = 2;
     // 1 = strict, 2 = relaxed, 3 = non-deterministic relaxed
+    private static int contiguity = 2;
 
-    int strategy = 1;
     // 1 = no skip, 2 = skip to next, 3 = skip past last event, 4 = skip to first, 5 = skip to last
+    private int strategy = 1;
+    private static AfterMatchSkipStrategy skipStrategy;
 
-    static AfterMatchSkipStrategy skipStrategy;
-    String patternName;
-    static String inputStr;
-    static boolean first = true;
-    static int nArgs = 0;
-
-    public Generate(String strP, int contiguity, StreamExecutionEnvironment env, int nArgs){
-        this.strP = strP.toLowerCase(Locale.ROOT).replaceAll("\\s+","");
-        this.contiguity = contiguity;
-        this.env = env;
-        this.nArgs = nArgs;
+    public Generate(String strP, int contiguity){
+        Generate.strP = strP.toLowerCase(Locale.ROOT).replaceAll("\\s+","");
+        Generate.contiguity = contiguity;
     }
 
     public void setStrP(String strP) {
-        this.strP = strP.toLowerCase(Locale.ROOT).replaceAll("\\s+","");
+        Generate.strP = strP.toLowerCase(Locale.ROOT).replaceAll("\\s+","");
     }
 
     public static String getStrP() {
@@ -67,22 +56,18 @@ public class Generate implements Serializable{
     }
 
     public void setNr(int nr) {
-        this.nr = nr;
+        Generate.nr = nr;
     }
 
     public static int getNr() {
         return nr;
     }
 
-    public static double getTime() {
-        return System.currentTimeMillis() - start;
-    }
-
     public void setContiguity(int contiguity) {
-        this.contiguity = contiguity;
+        Generate.contiguity = contiguity;
     }
 
-    public static String getContiguity() {
+    private static String getContiguity() {
         String cont = "";
         switch (contiguity){
             case 1: // Strict contiguity
@@ -99,8 +84,8 @@ public class Generate implements Serializable{
     }
 
     public void setStrategy(int strategy, String patternName) {
+        // patterName is only used in "skip to first / last" strategies
         this.strategy = strategy;
-        this.patternName = patternName;
         switch (strategy) {
             case 1: // No skip strategy
                 skipStrategy = AfterMatchSkipStrategy.noSkip();
@@ -120,7 +105,7 @@ public class Generate implements Serializable{
         }
     }
 
-    public String getStrategy() {
+    private String getStrategy() {
         String retStrategy = "";
         switch (strategy){
             case 1: // No skip strategy
@@ -143,30 +128,36 @@ public class Generate implements Serializable{
     }
 
     public void setN(int n) {
-        this.n = n;
+        Generate.n = n;
     }
 
     public static int getN() {
         return n;
     }
 
-    public static Pattern<Event, ?> createPattern() throws Exception {
+    // Create wanted pattern with the help of FlinkCEP library
+    public static Pattern<Event, ?> createPattern() {
         Pattern<Event, ?> resultP = null;
         char cur;
         String curStr, nextStr;
 
         for (int i = 0; i < strP.length(); i++){
-            cur = strP.charAt(i); // for each character of the sequence
+            // For each character of the sequence
+            cur = strP.charAt(i);
             if(Character.isLetter(cur)){
+                // Letter
                 curStr = Character.toString(cur);
-                if ((i + 1) < strP.length() && strP.charAt(i + 1) == '|'){ // letter OR letter
+                if ((i + 1) < strP.length() && strP.charAt(i + 1) == '|'){
+                    // Letter OR letter
                     i += 2;
                     nextStr = Character.toString(strP.charAt(i));
                     resultP = createOr(resultP, curStr, nextStr);
-                }else{ // letter
+                }else{
+                    // Letter (without OR)
                     resultP = createLetter(resultP, curStr);
                 }
-            }else if (cur == '+' || cur == '*'){ // + (one or more), * (zero or more)
+            }else if (cur == '+' || cur == '*'){
+                // + (one or more) or * (zero or more)
                 switch (contiguity){
                     case 1: // Strict contiguity
                         resultP = resultP.oneOrMore().consecutive();
@@ -181,31 +172,34 @@ public class Generate implements Serializable{
                 if (cur == '*'){
                     resultP = resultP.optional();
                 }
-            }else if (cur == '?'){ // ? (optional)
+            }else if (cur == '?'){
+                // ? (optional)
                 resultP = resultP.optional();
-            }else if(cur == '{') { // {low,high} (we want previous character from low to high times)
+            }else if(cur == '{') {
+                // {low,high} (we want the previous character from low to high times)
                 int low = Character.getNumericValue(strP.charAt(i + 1));
                 int high = Character.getNumericValue(strP.charAt(i + 3));
                 resultP = resultP.times(low, high);
-                i += 4; // go to }
+                i += 4; // Go to "}" and get ready for next event
             }
         }
         return resultP;
     }
 
-    public static Pattern<Event, ?> createLetter(Pattern<Event, ?> p, String ev){
+    // Add a letter to the pattern based on wanted conditions
+    private static Pattern<Event, ?> createLetter(Pattern<Event, ?> p, String ev){
         // Create wanted condition
         SimpleCondition<Event> cond = new SimpleCondition<Event>() {
             @Override
-            public boolean filter(Event value) throws Exception {
+            public boolean filter(Event value) {
                 return value.getValue().toLowerCase(Locale.ROOT).startsWith(ev);
             }
         };
 
-        n++; // increase number of events in the pattern
+        n++; // Increase number of events in the pattern
         String strN = Integer.toString(n); // Name of this event in the pattern
 
-        if(n == 1){ // first event
+        if(n == 1){ // First event
             // "begin" uses non-deterministic contiguity, that is, it will make matches starting with as many alternatives as possible
             p = Pattern.<Event>begin(strN, skipStrategy).where(cond);
         }else{
@@ -224,23 +218,24 @@ public class Generate implements Serializable{
         return p;
     }
 
-    public static Pattern<Event, ?> createOr(Pattern<Event, ?> p, String ev, String evNext){
+    // Create OR between two events based on wanted conditions (OR is only supported between two events)
+    private static Pattern<Event, ?> createOr(Pattern<Event, ?> p, String ev, String evNext){
         // Create wanted condition
         SimpleCondition<Event> cond = new SimpleCondition<Event>() {
             @Override
-            public boolean filter(Event value) throws Exception {
+            public boolean filter(Event value) {
                 return value.getValue().toLowerCase(Locale.ROOT).startsWith(ev);
             }
         };
 
-        n ++; // increase number of events in the pattern
+        n ++; // Increase number of events in the pattern (consider "event OR event" as one event)
         String strN = Integer.toString(n); // Name of this event in the pattern
 
         if(n == 1){ // first
             // "begin" uses non-deterministic contiguity, that is, it will make matches starting with as many alternatives as possible
             p = Pattern.<Event>begin(strN, skipStrategy).where(cond).or(new SimpleCondition<Event>() {
                 @Override
-                public boolean filter(Event value) throws Exception {
+                public boolean filter(Event value) {
                     return value.getValue().toLowerCase(Locale.ROOT).startsWith(evNext);
                 }
             });
@@ -249,7 +244,7 @@ public class Generate implements Serializable{
                 case 1: // Strict contiguity
                     p = p.next(strN).where(cond).or(new SimpleCondition<Event>() {
                         @Override
-                        public boolean filter(Event value) throws Exception {
+                        public boolean filter(Event value) {
                             return value.getValue().toLowerCase(Locale.ROOT).startsWith(evNext);
                         }
                     });
@@ -257,7 +252,7 @@ public class Generate implements Serializable{
                 case 2: // Relaxed contiguity
                     p = p.followedBy(strN).where(cond).or(new SimpleCondition<Event>() {
                         @Override
-                        public boolean filter(Event value) throws Exception {
+                        public boolean filter(Event value) {
                             return value.getValue().toLowerCase(Locale.ROOT).startsWith(evNext);
                         }
                     });
@@ -265,7 +260,7 @@ public class Generate implements Serializable{
                 case 3: // Non - deterministic contiguity
                     p = p.followedByAny(strN).where(cond).or(new SimpleCondition<Event>() {
                         @Override
-                        public boolean filter(Event value) throws Exception {
+                        public boolean filter(Event value) {
                             return value.getValue().toLowerCase(Locale.ROOT).startsWith(evNext);
                         }
                     });
@@ -275,12 +270,13 @@ public class Generate implements Serializable{
         return p;
     }
 
+    // Create result with the matching results
     public static DataStream<String> createResult(PatternStream<Event> patternStream){
         DataStream<String> result =  patternStream.select((Map<String, List<Event>> p) -> {
             String strResult = "";
 
             int test;
-            // In submitflink the n has not been updated 
+            // In submitflink the n has not been updated
             if (n > 0) {
                 test = n;
             } else{
@@ -291,15 +287,12 @@ public class Generate implements Serializable{
                 String strN = Integer.toString(i + 1);
                 // Check null because the optional() quantifier can be used
                 if (p.get(strN) != null) {
-                    for (int j = 0; j < p.get(strN).size(); j++) { // for looping patterns (+, *)
+                    for (int j = 0; j < p.get(strN).size(); j++) { // For looping patterns (+, *)
                         strResult += p.get(strN).get(j).getValue() + " ";
                     }
                 }
             }
-            if (first){ // Print time only the first time (all matches have already been found)
-                strResult += " (" + getTime() / 1000 + " sec)";
-                first = false;
-            }
+
             return nr + ". " + strResult;
         });
         return result;
@@ -308,17 +301,25 @@ public class Generate implements Serializable{
     @Override
     public String toString() {
         return "Wanted pattern: " + strP + " (Number of events: " + n + ")"
-                + "\nContiguity condition: " + this.getContiguity() + "\nAfter match strategy: "
-                + this.getStrategy() + "\nNumber of Arguments: " + nArgs + "\n\nMatching results:";
+                + "\nContiguity condition: " + getContiguity() + "\nAfter match strategy: "
+                + this.getStrategy() + "\n\nMatching results:";
     }
 
-    public static String readFile(String pathName){
+    // Read text file with data
+    private static ArrayList<String> readFile(String pathName){
         try {
+            ArrayList<String> data = new ArrayList<>();
             File file = new File(pathName);
             Scanner myReader = new Scanner(file);
-            String data = "";
             while (myReader.hasNextLine()) {
-                data += myReader.nextLine();
+                String lineStr = myReader.nextLine();
+                String[] line;
+                line = lineStr.split(",");
+                if(line[0] != "-1"){
+                    for (int i = 0; i < 3; i++){
+                        data.add(line[i].replaceAll("\\s+",""));
+                    }
+                }
             }
             myReader.close();
             return data;
@@ -329,18 +330,14 @@ public class Generate implements Serializable{
         }
     }
 
-    public static Collection<Event> createInput(String pathName) throws Exception {
+    // Create input from text file
+    public static Collection<Event> createInput(String pathName) {
         Collection<Event> events = new ArrayList<>();
-        inputStr = readFile(pathName);
-        String[] words = inputStr.split(",");
-
-        for (int i = 0; i < words.length; i++){
-            //events.add(new Event(Integer.parseInt(Character.toString(inputStr.charAt(i))), Integer.parseInt(Character.toString(inputStr.charAt(i + 2))), Character.toString(inputStr.charAt(i + 4))));
-            events.add(new Event(Integer.parseInt(words[i]), Integer.parseInt(words[i + 1]), words[i + 2]));
+        ArrayList<String> words = readFile(pathName);
+        for (int i = 0; i < words.size(); i++){
+            events.add(new Event(Integer.parseInt(words.get(i)), Integer.parseInt(words.get(i + 1)), words.get(i + 2)));
             i += 2;
-            //events.add(new Event(0, 0, Character.toString(inputStr.charAt(i))));
         }
-
         return events;
     }
 }
